ebb1fc67e6c8a058ee8711da9d1565b1c317fbd4
Tyler Race <tylerrrace@gmail.com>
2020-01-23 18:11:22 -0800

    Added blank test for null arg to textdumper

--- airline/src/test/java/edu/pdx/cs410J/race3/TextDumperTest.java
+++ airline/src/test/java/edu/pdx/cs410J/race3/TextDumperTest.java
@@ -1,4 +1,15 @@
 package edu.pdx.cs410J.race3;
 
+import org.junit.Test;
+
+import java.lang.IllegalArgumentException;
+import java.io.IOException;
+
 public class TextDumperTest {
+  TextDumper td = new TextDumper();
+
+  @Test (expected = IllegalArgumentException.class)
+  public void testNullPassedToDumpReturnsInvalidArgumentException() {
+    
+  }
 }
