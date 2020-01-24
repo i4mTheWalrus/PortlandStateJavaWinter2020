71905ede58002ba0601ac31b7192bfab449cef27
Tyler Race <tylerrrace@gmail.com>
2020-01-23 17:43:15 -0800

    Added javadoc comments for new classes

--- airline/src/main/java/edu/pdx/cs410J/race3/TextDumper.java
+++ airline/src/main/java/edu/pdx/cs410J/race3/TextDumper.java
@@ -5,8 +5,10 @@ import edu.pdx.cs410J.AirlineDumper;
 
 import java.io.IOException;
 
+/**
+ * Class that dumps the contents of an airline (including ﬂights) to a text ﬁle.
+ */
 public class TextDumper implements AirlineDumper {
-
   @Override
   public void dump(AbstractAirline abstractAirline) throws IOException {
 
--- airline/src/main/java/edu/pdx/cs410J/race3/TextParser.java
+++ airline/src/main/java/edu/pdx/cs410J/race3/TextParser.java
@@ -4,9 +4,12 @@ import edu.pdx.cs410J.AbstractAirline;
 import edu.pdx.cs410J.AirlineParser;
 import edu.pdx.cs410J.ParserException;
 
+/**
+ * A class that reads the contents of a text ﬁle and from it creates an airline with its associated ﬂights.
+ */
 public class TextParser implements AirlineParser {
   @Override
-  public AbstractAirline<?> parse() throws ParserException {
+  public AbstractAirline parse() throws ParserException {
     return null;
   }
 
