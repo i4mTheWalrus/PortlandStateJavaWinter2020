b677ef6b6b0119a25165dad332ee4d7d10ad8903
Tyler Race <tylerrrace@gmail.com>
2020-01-25 12:15:08 -0800

    Added test for TextParser to make sure that correct file path is created.

--- airline/src/main/java/edu/pdx/cs410J/race3/TextParser.java
+++ airline/src/main/java/edu/pdx/cs410J/race3/TextParser.java
@@ -16,7 +16,7 @@ public class TextParser implements AirlineParser {
 
   /**
    * Constructor that takes in a filename. Path is assumed to be in src/main/resources/
-   * @param fileName
+   * @param fileName Specify the name of the file. .txt extension should be given with filename.
    */
   TextParser(String fileName) {
     this.FileName = "src/main/resources/" + fileName;
@@ -26,16 +26,4 @@ public class TextParser implements AirlineParser {
   public AbstractAirline parse() throws ParserException {
     return null;
   }
-
-  /**
-   * Method takes a file path, checks that it exists, then calls parse.
-   * @param filePath
-   */
-  public AbstractAirline parseFile(String filePath) throws IllegalArgumentException {
-    // check that the file path is formatted correctly
-    // check that the file exists
-    // try to parse the text file, parse returns an airline object
-    return null;
-  }
-
 }
--- airline/src/test/java/edu/pdx/cs410J/race3/TextParserTest.java
+++ airline/src/test/java/edu/pdx/cs410J/race3/TextParserTest.java
@@ -1,7 +1,20 @@
 package edu.pdx.cs410J.race3;
 
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Class to unit test the {@link TextParser} class.
  */
 public class TextParserTest {
+
+  /**
+   * Test that TextParser creates the correct file in the correct directory (resources).
+   */
+  @Test
+  public void testConstructorCreatesCorrectFileAndFilePath() {
+    TextParser tp = new TextParser("testPath.txt");
+    assertThat(tp.FileName, is("src/main/resources/testPath.txt"));
+  }
 }
