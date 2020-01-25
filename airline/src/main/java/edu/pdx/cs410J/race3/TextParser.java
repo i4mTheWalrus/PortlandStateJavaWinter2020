a6b3b057b385abfb61778e8908cb6e065698da46
Tyler Race <tylerrrace@gmail.com>
2020-01-25 12:08:39 -0800

    Added filename var and constructor to TextParser class

--- airline/src/main/java/edu/pdx/cs410J/race3/TextParser.java
+++ airline/src/main/java/edu/pdx/cs410J/race3/TextParser.java
@@ -8,6 +8,20 @@ import edu.pdx.cs410J.ParserException;
  * A class that reads the contents of a text ﬁle and from it creates an airline with its associated ﬂights.
  */
 public class TextParser implements AirlineParser {
+
+  /**
+   * Variable of TextParser that holds the name of the file. Path is assumed to be in src/main/resources/
+   */
+  String FileName;
+
+  /**
+   * Constructor that takes in a filename. Path is assumed to be in src/main/resources/
+   * @param fileName
+   */
+  TextParser(String fileName) {
+    this.FileName = "src/main/resources/" + fileName;
+  }
+
   @Override
   public AbstractAirline parse() throws ParserException {
     return null;
