4ccedb282fe8c65d3b1dc06cb6031b33cc6bc75f
Tyler Race <tylerrrace@gmail.com>
2020-01-25 12:28:37 -0800

    Added test and cecking for file not found exception in TextParser

--- airline/src/main/java/edu/pdx/cs410J/race3/TextParser.java
+++ airline/src/main/java/edu/pdx/cs410J/race3/TextParser.java
@@ -4,6 +4,11 @@ import edu.pdx.cs410J.AbstractAirline;
 import edu.pdx.cs410J.AirlineParser;
 import edu.pdx.cs410J.ParserException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 /**
  * A class that reads the contents of a text ﬁle and from it creates an airline with its associated ﬂights.
  */
@@ -16,14 +21,27 @@ public class TextParser implements AirlineParser {
 
   /**
    * Constructor that takes in a filename. Path is assumed to be in src/main/resources/
-   * @param fileName Specify the name of the file. .txt extension should be given with filename.
+   * @param fileName Specify the name of the file. The file extension should be given with filename.
    */
   TextParser(String fileName) {
     this.FileName = "src/main/resources/" + fileName;
   }
 
+  /**
+   * Parses the text file referred to by constructor, stores is in an airline, and returns that airline.
+   * @return Airline containing flights in the file specified.
+   * @throws ParserException Thrown if text file is malformed.
+   */
   @Override
   public AbstractAirline parse() throws ParserException {
+    File inFile = new File(this.FileName);
+    // Check that the file exists (thrown exception by file reader if not)
+    try {
+      FileReader inReader = new FileReader(inFile);
+    } catch (FileNotFoundException e) {
+      throw new ParserException("File not found!");
+    }
+
     return null;
   }
 }
--- airline/src/test/java/edu/pdx/cs410J/race3/TextParserTest.java
+++ airline/src/test/java/edu/pdx/cs410J/race3/TextParserTest.java
@@ -1,6 +1,10 @@
 package edu.pdx.cs410J.race3;
 
+import edu.pdx.cs410J.ParserException;
 import org.junit.Test;
+
+import java.text.ParseException;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -15,6 +19,13 @@ public class TextParserTest {
   @Test
   public void testConstructorCreatesCorrectFileAndFilePath() {
     TextParser tp = new TextParser("testPath.txt");
-    assertThat(tp.FileName, is("src/main/resources/testPath.txt"));
+    assertThat(tp.FileName.contains("src/main/resources/"), is(true));
+  }
+
+  @Test (expected = ParserException.class)
+  public void testFileNotFoundThrowsParserException() throws ParserException {
+    TextParser tp = new TextParser("");
+    Airline airline = new Airline();
+    airline = (Airline)tp.parse();
   }
 }
