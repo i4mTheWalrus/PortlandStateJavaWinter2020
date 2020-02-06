f4339b66e81e9d9d9d56dd42f50dda28c4499f02
Tyler Race <tylerrrace@gmail.com>
2020-02-06 08:52:12 -0800

    Added test for pretty print to file

---
+++ airline/src/test/java/edu/pdx/cs410J/race3/PrettyPrinterTest.java
@@ -0,0 +1,26 @@
+package edu.pdx.cs410J.race3;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PrettyPrinterTest {
+
+  @Test
+  public void testPrettyOutputToStandardOut() throws IOException {
+    PrettyPrinter printer = new PrettyPrinter("TestPretty.txt");
+    Airline airline = new Airline("Tester");
+    Flight flight = new Flight("Tester", "234", "BOI", "2/2/2002", "4:30", "am", "PDX", "2/2/2002", "5:30", "am");
+    airline.addFlight(flight);
+    printer.dump(airline);
+    BufferedReader reader = new BufferedReader(new FileReader("TestPretty.txt"));
+    String line = reader.readLine();
+    assertThat(line.contains("Tester flight 234 departs Boise, ID at 2/2/02, 4:30 AM and arrives at Portland, OR at 2/2/02, 5:30 AM for a total flight time of 60 minutes."), is(true));
+    reader.close();
+  }
+}
