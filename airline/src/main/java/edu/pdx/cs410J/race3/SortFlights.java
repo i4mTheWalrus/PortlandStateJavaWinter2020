7d11bdee0484fe27ad7ac82e72710f3e54200dfb
Tyler Race <tylerrrace@gmail.com>
2020-02-05 18:56:52 -0800

    Added the sort class

---
+++ airline/src/main/java/edu/pdx/cs410J/race3/SortFlights.java
@@ -0,0 +1,12 @@
+package edu.pdx.cs410J.race3;
+
+import java.util.Comparator;
+
+public class SortFlights implements Comparator<Flight> {
+
+  @Override
+  public int compare(Flight flight1, Flight flight2) {
+    
+    return 0;
+  }
+}
