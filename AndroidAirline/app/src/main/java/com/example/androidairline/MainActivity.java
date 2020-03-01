16ebf9b462b62c7b927feac686c9b2f79a8332f8
Tyler Race <tylerrrace@gmail.com>
2020-03-01 11:30:33 -0800

    Popup help window added

--- AndroidAirline/app/src/main/java/com/example/androidairline/MainActivity.java
+++ AndroidAirline/app/src/main/java/com/example/androidairline/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.androidairline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button helpButton = (Button)findViewById(R.id.helpButton);
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HelpPopup.class));
+            }
+        });
     }
 }
